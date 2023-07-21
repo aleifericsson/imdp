7f3ed27e6746e3fae1ada697a120728d01903f84
aleifericsson <alifnafili@gmail.com>
2023-07-21 21:56:37 +0400

    more

--- code.js
+++ code.js
@@ -1,14 +1,40 @@
 let media_list = [];
 
-function Media(name, year, type, rating){
+function Media(name, year, creator, type, rating){
         this.name = name;
         this.year = year;
+        this.creator = creator;
         this.type = type;
         this.rating = rating;
 }
 
 function addMedia(media){
     media_list.push(media);
+    let stars = parseInt(media.rating);
+    let star_string = "";
+    for (let i=0;i<5;i++){
+        if (stars===0){
+            star_string = star_string+"☆";
+        }
+        else if (stars===1){
+            star_string = star_string+"⯪";
+            stars = stars-1;
+        }
+        else{
+            star_string = star_string+"★";
+            stars = stars-2;
+        }
+    }
+    const new_li = document.createElement("li");
+    new_li.innerHTML = `
+        <div class = "card-name">${media.name}</div>
+        <div class = "card-creator">${media.creator}</div>
+        <div class = "card-year">${media.year}</div>
+        <div class = "card-type">${media.type}</div>
+        <div class = "card-rating">${star_string}</div>
+    `;
+    new_li.classList.add("media-card");
+    media_disp.appendChild(new_li);
 }
 
 function createMedia(e){
@@ -17,6 +43,7 @@ function createMedia(e){
     const temp_media = new Media(
         name_in.value,
         year_in.value,
+        creator_in.value,
         type_in.value,
         rating_in.value
     );
@@ -31,9 +58,11 @@ function toggleForm(){
 const form = document.querySelector("#new-media");
 const name_in = document.querySelector("#name");
 const year_in = document.querySelector("#year"); 
+const creator_in = document.querySelector("#creator");
 const rating_in = document.querySelector("#rating");
 const submit = document.querySelector('#submit');
 submit.addEventListener("click", createMedia);
 
 const new_media_btn = document.querySelector("#new-media-btn");
 new_media_btn.addEventListener("click", toggleForm);
+const media_disp = document.querySelector(".media-disp");
