05f2fd6cd943bf5e624fe6c106765e46092189e9
aleifericsson <alifnafili@gmail.com>
2023-07-25 20:21:30 +0400

    final website complete

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-let media_list = [];
+/*
+    To Do:
+
+    - add a dynamic rating star thing so that when you slide the bar it changes the star
+    - add a delete button for cards
+    - use firebase or whatever to save what is created
+
+*/
+
+let card_list = [];
 
 function Media(name, year, creator, type, rating){
         this.name = name;
@@ -9,15 +18,41 @@ function Media(name, year, creator, type, rating){
 }
 
 function addMedia(media){
-    media_list.push(media);
     let stars = parseInt(media.rating);
+    const star_string = generateStars(stars);
+    const new_li = document.createElement("li");
+    new_li.innerHTML = `
+        <div>
+            <div class = "card-name">${media.name}</div>
+            <div class = "card-creator">${media.creator}</div>
+            <div class = "card-year">${media.year}</div>
+            <div class = "card-type">${media.type}</div>
+            <div class = "card-rating">${star_string}</div>
+        </div>
+        <div>
+            <button class="remove">Remove</button>
+        </div>
+    `;
+    new_li.classList.add("media-card");
+    media_disp.appendChild(new_li);
+    card_list.push(new_li);
+    const remove = new_li.querySelector(".remove");
+    remove.addEventListener("click",removeCard);
+}
+
+function removeCard(e){
+    const card = this.parentNode.parentNode;
+    card.remove();
+}
+
+function generateStars(stars){
     let star_string = "";
     for (let i=0;i<5;i++){
         if (stars===0){
             star_string = star_string+"☆";
         }
         else if (stars===1){
-            star_string = star_string+"⯪";
+            star_string = star_string+"&#x2BEA;";
             stars = stars-1;
         }
         else{
@@ -25,16 +60,7 @@ function addMedia(media){
             stars = stars-2;
         }
     }
-    const new_li = document.createElement("li");
-    new_li.innerHTML = `
-        <div class = "card-name">${media.name}</div>
-        <div class = "card-creator">${media.creator}</div>
-        <div class = "card-year">${media.year}</div>
-        <div class = "card-type">${media.type}</div>
-        <div class = "card-rating">${star_string}</div>
-    `;
-    new_li.classList.add("media-card");
-    media_disp.appendChild(new_li);
+    return star_string;
 }
 
 function createMedia(e){
@@ -55,7 +81,6 @@ function toggleForm(){
     form.classList.toggle("offscreen");
 }
 
-
 function toggleSonic(){
     if (sonic.classList.contains("SONIC")){
         location.reload();
@@ -73,9 +98,17 @@ function toggleSonic(){
         header.setAttribute('style','background:#6663FF;');
         const thesonic = document.createElement("img");
         thesonic.setAttribute("src","./images/Portrait_sonic_winking.png");
-        thesonic.style="height:auto; width:290px; left:5px; position:absolute; bottom:0px;";
+        thesonic.style="height:auto; width:290px; margin-left: 5px;";
+        const sHADOW = document.createElement("img");
+        sHADOW.setAttribute("src","./images/shadow.png");
+        sHADOW.style="height:auto; width:290px; position:relative; top:500px;";
+        sidebar.appendChild(sHADOW);
         sidebar.appendChild(thesonic);
     }
+}
+
+function updateRating(e){
+    rat_lab.innerHTML = "Rating: " + generateStars(parseInt(rating_in.value));
 }
 
 const endpoint = './sonic.json';
@@ -91,11 +124,15 @@ const creator_in = document.querySelector("#creator");
 const rating_in = document.querySelector("#rating");
 const submit = document.querySelector('#submit');
 submit.addEventListener("click", createMedia);
+rating_in.addEventListener("input", updateRating)
 
 const new_media_btn = document.querySelector("#new-media-btn");
 new_media_btn.addEventListener("click", toggleForm);
+const cancel = document.querySelector("#cancel");
+cancel.addEventListener("click", toggleForm);
 const media_disp = document.querySelector(".media-disp");
 const sonic = document.querySelector("#SONIC");
 sonic.addEventListener("click", toggleSonic);
 const header = document.querySelector(".header");
 const sidebar = document.querySelector(".sidebar");
+const rat_lab = document.querySelector("#rating-label");
